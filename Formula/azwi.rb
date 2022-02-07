892e14d460f96d920036fdc9116c73eb8b28f794
Ernest Wong <chuwon@microsoft.com>
2022-02-07 15:47:40 -0800

    chore: bump azwi to v0.8.0
    Signed-off-by: Ernest Wong <chuwon@microsoft.com>

--- Formula/azwi.rb
+++ Formula/azwi.rb
@@ -4,27 +4,27 @@
 class Azwi < Formula
   desc "A utility CLI that helps manage Azure AD Workload Identity and automate error-prone operations:"
   homepage "https://azure.github.io/azure-workload-identity"
-  version "0.7.0"
+  version "0.8.0"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/Azure/azure-workload-identity/releases/download/v#{version}/azwi-v#{version}-darwin-amd64.tar.gz"
-      sha256 "d1dd0e91980b411de42a2f32fad295e8d7601b85845071d35ef5f18af0d68b5b"
+      sha256 "5359db2c3e69d082ec3789624e8b7ec529e442e61a3129afad01fa5606e57e06"
     end
     if Hardware::CPU.arm?
       url "https://github.com/Azure/azure-workload-identity/releases/download/v#{version}/azwi-v#{version}-darwin-arm64.tar.gz"
-      sha256 "63ae9a139e2853e0ca194877fb99eb68e6930142f6f9c67be3836c2f7f422a28"
+      sha256 "de3278c8449d7e23a3a3defc179cbd8e9c9eac43a06bd067d44d27bc411d37a4"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/Azure/azure-workload-identity/releases/download/v#{version}/azwi-v#{version}-linux-amd64.tar.gz"
-      sha256 "9d3bf21cc105cd96db9a6f88dc8d44a1c34061bef6127a18cfaddca8ebc6554a"
+      sha256 "93dbca819b2aa44b37a64a928361da68c5b8b720acf88c1371b45050ff4ab248"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/Azure/azure-workload-identity/releases/download/v#{version}/azwi-v#{version}-linux-arm64.tar.gz"
-      sha256 "40bcc6be3758b9b6d08e45e0bd582403265634b2e804c712388d2fb6509217c4"
+      sha256 "2e778c35606843a0a78347e7a4d04a639143ab7043cdac53edd058434fa921ad"
     end
   end
 
